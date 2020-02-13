65ecdd92524c926296e1fcd8d4878d35e00d851f
Leigh Brenecki <l@leigh.net.au>
2020-02-13 19:10:30 +1030

    Use configured homepage as new tab page

--- newtab.js
+++ newtab.js
@@ -1,10 +1,17 @@
 ;(async function() {
+  // Find out what the homepage is set to
+  const homepageRaw = (await browser.browserSettings.homepageOverride.get({}))
+    .value
+  const homepage = /^https?:\/\//.test(homepageRaw)
+    ? homepageRaw
+    : "http://" + homepageRaw
+  document.querySelector("iframe").src = homepage
+
+  // When we navigate away from the homepage, navigate the top level instead
   const currTab = await browser.tabs.getCurrent()
-  console.log(currTab)
   browser.webNavigation.onBeforeNavigate.addListener(e => {
     console.log(e)
     if (e.tabId === currTab.id && e.frameId !== 0 && e.parentFrameId === 0) {
-      console.log("loading", e.url)
       window.location.href = e.url
     }
   })
